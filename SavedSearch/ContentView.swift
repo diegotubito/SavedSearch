901908b7e3e090128ae15d6ededc841acb37ae97
David Diego Gomez <diego@MBP-de-David.fibertel.com.ar>
2023-11-04 02:35:19 -0300

    first commit

---
+++ SavedSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SavedSearch
+//
+//  Created by David Diego Gomez on 04/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SavedSearch/SavedSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavedSearchApp.swift
+//  SavedSearch
+//
+//  Created by David Diego Gomez on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SavedSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
